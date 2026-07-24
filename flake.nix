b49ea715df3346f38177592fd6fe20fd3b8846fb
Noah Martinez <67680002+Noah-Martinez@users.noreply.github.com>
2026-07-24 22:44:44 +0200

    build: expose flake checks and dev shell

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "CLI to watch Movies/TV Shows from the terminal";
+  description = "Maintained fork of the Lobster movie and TV streaming CLI";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -24,6 +24,21 @@
       lobster = pkgsFor.${system}.callPackage ./default.nix {};
       default = self.packages.${system}.lobster;
     });
+
+    checks = eachSystem (system: {
+      package = self.packages.${system}.lobster;
+      version = self.packages.${system}.lobster.passthru.tests.version;
+    });
+
+    devShells = eachSystem (system: {
+      default = pkgsFor.${system}.mkShellNoCC {
+        packages = with pkgsFor.${system}; [
+          shellcheck
+          shfmt
+        ];
+      };
+    });
+
+    formatter = eachSystem (system: pkgsFor.${system}.nixpkgs-fmt);
   };
 }
-
